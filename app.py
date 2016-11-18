2b9a02fc6c1ec2d3d7e931ddf44e1efed8edebae
Christophe Graniczny <christophe.graniczny@gmail.com>
2016-11-18 23:35:01 +0000

    :sparkles: basic home template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, me!'
+def home():
+    return render_template('home.html')
