0ddda1211e4355a2f76e6c8dfe22856e55e99c09
Christophe Graniczny <christophe.graniczny@gmail.com>
2016-11-19 00:49:49 +0000

    :construction: building my api class

---
+++ gitmojithis/github_api.py
@@ -0,0 +1,11 @@
+
+class GithubRepo:
+    pass
+    # def __init__(self, root_path, defaults=None):
+    #     dict.__init__(self, defaults or {})
+    #     self.root_path = ''
+    def scan(self, repo):
+        return self
+
+    def exists(self):
+        return False
