598ca489b2a08ab10e484c04fccdc9c2489a1e7a
Christophe Graniczny <christophe.graniczny@gmail.com>
2016-11-19 00:48:52 +0000

    :sparkles: render a 404 if repo does not exist

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from gitmojithis import GithubRepo
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    stats = GithubRepo().scan('https:/github.com/krzystof/gitmojilint')
+    if not stats.exists():
+        return render_template('404.html')
+    return render_template('home.html', stats = stats)
